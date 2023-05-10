e47fb273bd0c4360c69d0941e13ec5e74998a76e
onurcihangir <onurcihangir1912@gmail.com>
2023-05-10 19:27:33 +0300

    assign app next page

--- src/pages/applications/AssignAppsNext.js
+++ src/pages/applications/AssignAppsNext.js
@@ -1,26 +1,24 @@
-import React, {useCallback, useEffect} from "react";
+import React, { useCallback, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import { Divider } from "@material-ui/core";
+import { Avatar, Divider, TextField } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
+import { TextArea } from "../../components/Fields/TextField";
 import Request from "../../helpers/Request";
+import fbLogo from "../../assets/Facebook_Logo_(2019).png";
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
+    paddingTop: 5,
     paddingLeft: "40px",
     paddingRight: "40px",
   },
   root: {
-    display: "flex",
-    flexDirection: "column",
+    // display: "flex",
+    // flexDirection: "column",
     // overflowY: "auto",
     maxHeight: "700px",
     margin: "0 auto",
@@ -31,15 +29,16 @@ const useStyles = makeStyles((theme) => ({
     borderWidth: "thin",
     borderRadius: "10px",
   },
+  header: {
+    fontWeight: "bold",
+  },
   typo: {
     fontWeight: "bold",
+    padding: 10,
   },
   buttons: {
     display: "flex",
     justifyContent: "flex-end",
-  },
-  submit: {
-    margin: "10px 1rem 0",
   },
   cancelButton: {
     backgroundColor: "white",
@@ -52,17 +51,29 @@ const useStyles = makeStyles((theme) => ({
     margin: "10px 1rem 0",
   },
   box: {
-    display: "flex",
+    // display: "flex",
+    // flexDirection: "row",
+    marginLeft: "40px",
+    marginRight: "40px",
     backgroundColor: "#D9D9D9",
     border: "solid",
     borderWidth: "2px",
     borderRadius: 5,
     // width: 359,
   },
-  formLabel: {
-    padding: 10,
-    fontWeight: "bold",
-    color: "black",
+  appnameRow: {
+    display: "flex",
+    flexDirection: "row",
+    paddingLeft: 15,
+    paddingBottom: 5,
+  },
+  avatar: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  appname: {
+    fontSize: "15px",
+    paddingLeft: 5,
   },
   divider: {
     height: 1,
@@ -70,8 +81,10 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: -3,
     backgroundColor: "black",
   },
-  formControl: {
-    margin: 3,
+  textField: {
+    marginTop: 5,
+    whiteSpace: "pre-wrap",
+    backgroundColor: "white",
   },
 }));
 
@@ -92,7 +105,7 @@ export default function AssignAppsNext() {
 
   const getApps = useCallback(async () => {
     const allApps = await Request("get", "/requestAppDisplay");
-    console.log(allApps)
+    console.log(allApps);
     setApps(allApps.data);
   }, []);
 
@@ -103,31 +116,88 @@ export default function AssignAppsNext() {
 
   return (
     <div className={classes.root}>
-      <Typography className={classes.typo} variant="h5" gutterBottom>
+      <Typography className={classes.header} variant="h5" gutterBottom>
         Assign Applications
       </Typography>
 
-      <Grid
+      {/* <Grid
         container
         spacing={6}
         className={classes.gridContainer}
         justifyContent="center"
       >
-        <Grid item xs={12} sm={6} md={12}>
-          <Box className={classes.box}>
-            <Typography>
-                Enter User Attributes
+        <Grid item xs={12} sm={6} md={12}> */}
+      <Box className={classes.box}>
+        <Typography className={classes.typo}>Enter User Attributes</Typography>
+        <div className={classes.appnameRow}>
+          <Avatar className={classes.avatar} alt="app" src={fbLogo} />
+          <Typography className={classes.appname}>Facebook</Typography>
+        </div>
+        <Divider />
+        <Grid
+          container
+          spacing={1}
+          className={classes.gridContainer}
+          justifyContent="center"
+        >
+          <Grid item xs={12} sm={6} md={6}>
+            <Typography className={classes.appname}>Person</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6}>
+            <Typography className={classes.appname}>
+              User Spesific Fields
             </Typography>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
+        <Divider />
+        <Grid
+          container
+          spacing={3}
+          className={classes.gridContainer}
+          justifyContent="center"
+        >
+          <Grid item xs={12} sm={6} md={6}>
+            <Typography className={classes.appname}>Person</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6}>
+            <TextArea //User NAME
+              label={"User Name"}
+              // value={""}
+              onChangeFunc={(value) => {
+                // setAccountProps({
+                //   ...accountProps,
+                //   userName: value.target.value,
+                // });
+              }}
+              style={classes.textField}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              label={"Password"}
+              type="password"
+              onChange={(value) => {
+                // setAccountProps({
+                //   ...accountProps,
+                //   password: value.target.value,
+                // });
+              }}
+              className={classes.textField}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      {/* </Grid>
+      </Grid> */}
       <div className={classes.buttons}>
         <Button
           type="submit"
           fullWidth
           variant="outlined"
           color="primary"
-        //   onClick={() => history.push("/apps")}
+          //   onClick={() => history.push("/apps")}
           className={classes.cancelButton}
         >
           Cancel
